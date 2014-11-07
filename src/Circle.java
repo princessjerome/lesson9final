ec6a62b21facb29aada86c0da2cab37537b79d7d
jere1330 <jere1330@ISS147D8-04.scdsb.pri>
2014-11-07 09:15:30 -0500

    Initial classes

---
+++ src/Circle.java
@@ -0,0 +1,9 @@
+public class Circle implements Shape{
+
+    @Override
+    public double area() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+    
+}
---
+++ src/Rect.java
@@ -0,0 +1,8 @@
+public class Rect implements Shape{
+
+    @Override
+    public double area() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

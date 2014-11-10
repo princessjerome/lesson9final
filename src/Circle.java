748146dc84d2c500606567cc8e0fada4658dba23
jere1330 <jere1330@ISS147D8-04.scdsb.pri>
2014-11-10 09:18:21 -0500

    Worked on shapes

--- src/Circle.java
+++ src/Circle.java
@@ -1,9 +1,54 @@
+
+import TurtleGraphics.Pen;
+
 public class Circle implements Shape{
 
-    @Override
-    public double area() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
+    private double xPos, yPos, radius;
+    
+    public Circle(){
+        xPos=0;
+        yPos=0;
+        radius=1;
     }
     
+    public Circle(double x,double y, double r){
+        xPos = x;
+        yPos = y;
+        radius = r;
+    }
+    
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw(Pen p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getXPos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getYPos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void move(double xLoc, double yLoc) {
+        xPos = xLoc;
+        yPos = yLoc;
+    }
+    
+    public void stretchBy(double factor) {
+        radius *= factor;
+    }
+    
+    public String toString(){
+        String str = "This is a CIRCLE\n";
+        str += "XLOC: " + xPos + "\tYLOC: " + yPos + "\n";
+        str += "RADIUS: " + radius + "\nAREA: " + area();
+        return str;
+    }
 }
--- src/Rect.java
+++ src/Rect.java
@@ -1,8 +1,67 @@
+
+import TurtleGraphics.Pen;
+
 public class Rect implements Shape{
 
-    @Override
-    public double area() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private double xPos, yPos, width, height;
+    
+    public Rect(){
+        xPos=0;
+        yPos=0;
+        width=1;
+        height=1;
     }
     
+    public Rect(double x,double y, double w, double h){
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
+    
+    public double area() {
+        return width * height;
+    }
+
+    public void draw(Pen p) {
+        p.up();
+        p.move(xPos, yPos);
+        p.down();
+        p.setDirection(0);
+        p.move(width);
+        p.turn(90);
+        p.move(height);
+        p.turn(90);
+        p.move(width);
+        p.turn(90);
+        p.move(height);
+    }
+
+    @Override
+    public double getXPos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getYPos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void move(double xLoc, double yLoc) {
+        xPos = xLoc;
+        yPos = yLoc;
+    }
+
+    public void stretchBy(double factor) {
+        height *= factor;
+        width *= factor;
+    }
+    
+    public String toString(){
+        String str = "This is a CIRCLE\n";
+        str += "XLOC: " + xPos + "\tYLOC: " + yPos + "\n";
+        str += "Width: " + width + "Height: " + height;
+        return str;
+    }
+        
 }
